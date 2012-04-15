9bb6bcbdef860947c197cc453785038eda3371bb
Tim Caswell <tim@creationix.com>
2012-04-15 00:16:47 -0500

    Clean up library

--- safereturn.js
+++ safereturn.js
@@ -1,17 +1,23 @@
 module.exports = safeReturn;
-// Wrapper to make sure a callback is only fired once, but eventually fires
+// Default timeout to a second.
+safeReturn.defaultTimeout = 1000;
 function safeReturn(callback, timeout) {
-  if (typeof callback !== "function") throw new Error("Callback must be function");
-  var isDone;
-  timeout = timeout || 60000;  // Nice slow default timeout
-  var timer = setTimeout(function () {
-    isDone = true;
-    callback(new Error("Callback timeout exceeded"));
-  }, timeout);
-  return function callback() {
+  if (typeof callback !== "function") throw new TypeError("Callback must be function");
+  var isDone, timer;
+  if (timeout === undefined) timeout = safeReturn.defaultTimeout;
+  if (timeout) {
+    // Create the error early to catch the interesting call stack
+    var timeoutErr = new Error("Callback timeout exceeded");
+    timer = setTimeout(function () {
+      isDone = true;
+      callback(timeoutErr);
+    }, timeout);
+  }
+  return function safeCallback() {
     if (isDone) return;
     clearTimeout(timer);
     isDone = true;
     return callback.apply(this, arguments);
   };
 }
+
